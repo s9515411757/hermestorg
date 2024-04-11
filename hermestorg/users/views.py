30c8e6395a321e540879e44385893ce689f21485
Александр Ершов <s9515411757@gmail.com>
2024-04-11 18:19:00 +0300

    Баг поправил с отправкой документов

--- hermestorg/users/views.py
+++ hermestorg/users/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import LoginForm
+from .models import User
 
 
 def login(request):
+
     if request.method == 'POST':
-        pass
+        form = LoginForm(request.POST, request.FILES)
+        if form.is_valid():
+            # first_name = form.cleaned_data['first_name']
+            # last_name = form.cleaned_data['last_name']
+            # username = form.cleaned_data['username']
+            # email = form.cleaned_data['email']
+            # flag_legal_entity = form.cleaned_data['flag_legal_entity']
+            # ogrn = form.cleaned_data['ogrn']
+            # document = form.cleaned_data['document']
+            form.save()
+            return redirect('home:index')
 
     form = LoginForm()
+
     return render(request, 'users/login.html', {'form': form})
