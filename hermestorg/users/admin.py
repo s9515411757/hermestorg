745750540815e6c67311a43a1866bef7f4903e7a
Александр Ершов <s9515411757@gmail.com>
2024-04-11 18:50:04 +0300

    Правки

--- hermestorg/users/admin.py
+++ hermestorg/users/admin.py
@@ -3,8 +3,8 @@ from .models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('flag_legal_entity', 'ogrn', 'document')
-    search_fields = ('ogrn',)
+    list_display = ('first_name', 'last_name', 'username', 'email', 'flag_legal_entity', 'ogrn', 'document')
+    search_fields = ('first_name', 'last_name', 'username', 'email', 'flag_legal_entity', 'ogrn',)
     list_filter = ('pub_date',)
 
 
